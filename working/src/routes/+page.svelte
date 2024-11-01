1265d495e2c4dd9b8cbf17148b8b93ad869ddda0
lixel_ <133605174+lixelv@users.noreply.github.com>
2024-11-01 19:10:44 +0300

    first commit

---
+++ working/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button builders={[builder]}>Trigger</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			<DropdownMenu.Label>My Account</DropdownMenu.Label>
+			<DropdownMenu.Separator />
+			<DropdownMenu.Item>Profile</DropdownMenu.Item>
+			<DropdownMenu.Item>Billing</DropdownMenu.Item>
+			<DropdownMenu.Item>Team</DropdownMenu.Item>
+			<DropdownMenu.Item>Subscription</DropdownMenu.Item>
+		</DropdownMenu.Group>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
